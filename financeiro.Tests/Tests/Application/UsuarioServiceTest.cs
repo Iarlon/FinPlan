208f4b1a0d5dbc4474cf321d257b9ebdde332fca
iarlon anima <iarlonpvh@gmail.com>
2026-05-24 17:51:10 -0300

    fix: ajuste de teste unitário de login de usuário

--- financeiro.Tests/Tests/Application/UsuarioServiceTest.cs
+++ financeiro.Tests/Tests/Application/UsuarioServiceTest.cs
@@ -1,45 +1,71 @@
 namespace financeiro.Tests.Application;
 
+using financeiro.Application.Contract;
+using financeiro.Domain.Repository;
+using Financeiro.Application.Command;
+using Financeiro.Application.Handles;
+using Financeiro.Domain.Exceptions;
+using Financeiro.Infraestructure.Database;
 using Moq;
 using Xunit;
-using financeiro.Domain.Service;
-using financeiro.Domain.Repository;
-using financeiro.Application.Contract;
-using financeiro.Infraestructure;
-using financeiro.Domain.Request;
 
 public class UsuarioServiceTest
 {
-    private readonly Mock<IUsuarioRepository> _usuarioRepositoryMock;
-    private readonly Mock<IHashService> _hashServiceMock;
-    private readonly CriarUsuario _service;
+    private readonly Mock<IUsuarioRepository>
+        _usuarioRepositoryMock;
+
+    private readonly Mock<IHashService>
+        _hashServiceMock;
+
+    private readonly Mock<IUnitOfWork>
+        _uowMock;
+
+    private readonly CriarUsuarioHandler _handler;
 
     public UsuarioServiceTest()
     {
-        _usuarioRepositoryMock = new Mock<IUsuarioRepository>();
-        _hashServiceMock = new Mock<IHashService>();
-        _service = new CriarUsuario(_usuarioRepositoryMock.Object, _hashServiceMock.Object);
+        _uowMock = new Mock<IUnitOfWork>();
+
+        _usuarioRepositoryMock =
+            new Mock<IUsuarioRepository>();
+
+        _hashServiceMock =
+            new Mock<IHashService>();
+
+        _handler = new CriarUsuarioHandler(
+            _usuarioRepositoryMock.Object,
+            _hashServiceMock.Object,
+            _uowMock.Object);
     }
 
     [Fact]
-    public async Task CriarUsuario_EmailJaExiste_DeveLancarException()
+    public async Task
+        CriarUsuario_EmailJaExiste_DeveLancarException()
     {
-        var request = new CriarUsuarioRequest
-        {
-            Nome = "Iarlon",
-            Email = "ialon@email.com",
-            Senha = "123456"
-        };
+        // Arrange
+
+        var command = new CriarUsuarioCommand(
+            "Iarlon",
+            "ialon@email.com",
+            "123456");
 
         _usuarioRepositoryMock
-            .Setup(x => x.EmailJaExiste(request.Email))
+            .Setup(x =>
+                x.EmailJaExiste(command.Email))
             .ReturnsAsync(true);
 
-        var exception = await Assert.ThrowsAsync<Exception>(() =>
-            _service.CriarUsuarioAsync(request)
-        );
+        // Act
 
-        Assert.Equal("E-mail já cadastrado.", exception.Message);
+        var exception =
+            await Assert.ThrowsAsync<DomainException>(
+                () => _handler.Handle(
+                    command,
+                    CancellationToken.None));
+
+        // Assert
+
+        Assert.Equal(
+            "E-mail já cadastrado.",
+            exception.Message);
     }
-
 }
